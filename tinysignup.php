bba6ea78bd02190cba2255801bf9c9587d67a1b9
Chris McCormick <chris@mccormick.cx>
2019-03-05 20:44:51 +0800

    CORS so you can link form from elsewhere.

--- tinysignup.php
+++ tinysignup.php
@@ -14,6 +14,8 @@ if (!$config) {
 
 /*** Routes ***/
 
+cors();
+
 if (isset($_GET["list"])) {
   if (isset($config["lists"][$_GET["list"]])) {
     header("Content-type: application/javascript");
@@ -165,6 +167,23 @@ function my_url() {
     return $protocol."://".$_SERVER['SERVER_NAME'].$port.$_SERVER['REQUEST_URI'];
 }
 function strleft($s1, $s2) { return substr($s1, 0, strpos($s1, $s2)); }
+
+function cors() {
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+        exit(0);
+    }
+}
 
 function see($var) {
   file_put_contents("php://stderr", print_r(func_get_args(), True) . "\n");
