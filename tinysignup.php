87165f38b97ec0956f0cdc4072d8849ba4a1adf4
Chris McCormick <chris@mccormick.cx>
2019-04-16 16:18:52 +0800

    Prevent errors if method is unset (e.g. cli tests).

--- tinysignup.php
+++ tinysignup.php
@@ -176,7 +176,7 @@ function cors() {
         header('Access-Control-Max-Age: 86400');    // cache for 1 day
     }
     // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
             header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
